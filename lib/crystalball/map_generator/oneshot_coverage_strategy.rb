f36c8a80d38377ea7c509988fddbef98a131373f
Andrejs <acunskis@gitlab.com>
2025-05-19 09:53:58 +0300

    Ensure starting coverage does not raise error
    Changelog: fix

--- lib/crystalball/map_generator/oneshot_coverage_strategy.rb
+++ lib/crystalball/map_generator/oneshot_coverage_strategy.rb
@@ -10,6 +10,7 @@ module Crystalball
     # Map generator strategy based on harvesting Coverage information during example execution
     class OneshotCoverageStrategy
       include BaseStrategy
+      include Logging
       include Helpers::PathFilter
 
       def after_register
@@ -21,10 +22,21 @@ module Crystalball
       # @param [Crystalball::ExampleGroupMap] example_map - object holding example metadata and used files
       # @param [RSpec::Core::Example] example - a RSpec example
       def call(example_map, example)
-        Coverage.start(oneshot_lines: true)
+        start_coverage
         yield example_map, example
         paths = Coverage.result.keys
         example_map.push(*filter(paths))
+      end
+
+      # Start coverage or restart it if it was already started
+      #
+      # @return [void] <description>
+      def start_coverage
+        Coverage.start(oneshot_lines: true) unless Coverage.running?
+
+        log(:warn, "[Crystalball] Coverage has been already started, restarting coverage for oneshot_lines!")
+        Coverage.result(stop: true, clear: true)
+        Coverage.start(oneshot_lines: true)
       end
     end
   end
