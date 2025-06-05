c0f65398792b7ce6a9a4aeaa495f15b76b46331d
Andrejs Cunskis <acunskis@gitlab.com>
2025-06-05 10:27:27 +0300

    Add support for regexp patterns for path filter
    Changelog: feature

--- spec/map_generator/helpers/path_filter_spec.rb
+++ spec/map_generator/helpers/path_filter_spec.rb
@@ -3,15 +3,17 @@
 require "spec_helper"
 
 describe Crystalball::MapGenerator::Helpers::PathFilter do
-  subject(:helper) { Class.new.tap { |c| c.include described_class }.new(root) }
+  subject(:path_filter) { Class.new.tap { |c| c.include described_class } }
+
+  let(:exclude_prefixes) { [] }
   let(:root) { "/foo" }
-  let(:paths) { ["/foo/file.rb", "/abc/file1.rb"] }
+  let(:paths) { ["/foo/file.rb", "/abc/file1.rb", "/foo/file_1.rb", "/foo/filtered_file.rb"] }
 
-  describe "#filter" do
-    subject { helper.filter(paths) }
+  it "returns only paths relative too root" do
+    expect(path_filter.new(root).filter(paths)).to eq(%w[file.rb file_1.rb filtered_file.rb])
+  end
 
-    it "takes paths relative too root only" do
-      is_expected.to eq(%w[file.rb])
-    end
+  it "returns filtered paths" do
+    expect(path_filter.new(root, exclude_prefixes: ["file_1.rb", /filtered/]).filter(paths)).to eq(%w[file.rb])
   end
 end
